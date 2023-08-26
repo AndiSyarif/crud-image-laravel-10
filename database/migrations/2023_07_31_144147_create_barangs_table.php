1c78ea4b3e6122b8e8c37d225ef1183f6b43f4cf
Andis Dev <andisyarif99@gmail.com>
2023-08-26 15:50:20 +0700

    Initial Commit
    Done

---
+++ database/migrations/2023_07_31_144147_create_barangs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->increments('id_barang');
+            $table->string('name', 100);
+            $table->string('category', 50);
+            $table->string('supplier', 100);
+            $table->string('stock', 5);
+            $table->string('price', 50);
+            $table->string('image', 100)->nullable();
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
